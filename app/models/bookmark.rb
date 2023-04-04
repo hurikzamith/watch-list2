6b374e921bb1d2d1297016f4d6b3044a682a1ab4
hurik zamith <hurikzamith@gmail.com>
2023-04-04 08:35:01 -0300

    seed

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :movie, presence: true, uniqueness: { scope: :list }
+  validates :comment, presence: true, length: { minimum: 6 }
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,6 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
-
+  validates :name, uniqueness: true, presence: true
 
 end
