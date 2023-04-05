47d31587dcbd51738c41d4228b004462212a8805
hurik zamith <hurikzamith@gmail.com>
2023-04-05 11:21:10 -0300

    new create index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,28 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+      if @list.save
+        redirect_to lists_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
