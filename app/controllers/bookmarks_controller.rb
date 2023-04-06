bd974fc03438385e731946c606f4efe81ea0a8b7
hurik zamith <hurikzamith@gmail.com>
2023-04-05 22:08:18 -0300

    bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,5 +2,18 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @list = @bookmark.list
   end
+
+  def create
+    @bookmark = Bookmark.new(params)
+    @list = @bookmark.list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+# TODO FIX CONTROLLER 
 end
